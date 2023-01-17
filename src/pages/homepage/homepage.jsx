abfac16edbdadc1230ccfd82d2d1ec81cdb80797
matincodes <abdulmatinadeniji@gmail.com>
2023-01-17 19:45:30 +0100

    uploading smaller code components

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,12 @@
+import "./homepage.scss";
+import Directory from "../../components/directory/directory";
+
+const Homepage =  () => {
+    return(
+        <div className="homepage">
+           <Directory />
+        </div>
+    )
+}
+
+export default Homepage;
